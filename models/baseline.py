0d41b0e43aa51d94e4264fccca60fe334f2467f7
prajwal9680 <prajwal9680@gmail.com>
2026-02-19 16:15:34 +0530

    added resnet

--- models/baseline.py
+++ models/baseline.py
@@ -1,35 +1,26 @@
 import torch
 import torch.nn as nn
-
+from models.resnet_block import ResidualBlock
 
 class BaselineCNN(nn.Module):
-    
     def __init__(self):
         super().__init__()
 
-        self.features = nn.Sequential(
-            nn.Conv2d(3,32, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
+        self.layer1 = nn.Sequential(nn.Conv2d(3, 32, kernel_size=3, padding=1), nn.BatchNorm2d(32), nn.ReLU(), nn.MaxPool2d(2))
+        self.layer2 = nn.Sequential(ResidualBlock(32, 64), nn.MaxPool2d(2))
+        self.layer3 = nn.Sequential(ResidualBlock(64, 128), nn.MaxPool2d(2))
 
-            nn.Conv2d(32, 64, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-
-            nn.Conv2d(64, 128, kernel_size = 3, padding = 1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-                                        )
-        
-        self.classifier = nn.Linear(128 * 4 * 4, 10)
+        self.classifier = nn.Linear(128 * 4 *4, 10)
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
         x = torch.flatten(x, 1)
+
         x = self.classifier(x)
+
         return x
 
     
